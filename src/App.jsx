112773378edf375c5717593fe05693f5ece544ce
arpitbansal08 <101734269+arpitbansal08@users.noreply.github.com>
2024-03-20 00:17:19 +0530

    fixing issue routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import { BrowserRouter, Route, Routes,HashRouter } from "react-router-dom";
 import "./App.css";
+
 function App() {
   return (
     <HashRouter>
-
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
