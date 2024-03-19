b3b586b29e18cff4e5547ce2b18909431175c6dc
arpitbansal08 <101734269+arpitbansal08@users.noreply.github.com>
2024-03-19 23:04:41 +0530

    Updated browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes,HashRouter } from "react-router-dom";
 import "./App.css";
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
