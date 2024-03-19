92c781263c2d6e6402b0834eabf17cce6591852b
arpitbansal08 <101734269+arpitbansal08@users.noreply.github.com>
2024-03-20 00:28:34 +0530

    Routing vite issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import { BrowserRouter, Route, Routes,HashRouter } from "react-router-dom";
+import { Route, Routes,HashRouter } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/#/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </HashRouter>
   );
