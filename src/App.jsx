676b89c3ad83ae6d7bcb5d7d3d46311278e1be19
arpitbansal08 <101734269+arpitbansal08@users.noreply.github.com>
2024-03-19 23:23:28 +0530

    Updated browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import { BrowserRouter, Route, Routes,HashRouter } from "react-router-dom";
 import "./App.css";
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </HashRouter>
+  
 );
